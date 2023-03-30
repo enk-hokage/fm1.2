f38cdcee0327cad52cde7dc373b1eecb7128aedf
Danyil <enk.hokage2@gmail.com>
2023-03-30 14:30:18 +0300

    Fix

---
+++ Lesson1/src/main/java/ConsoleReader.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ConsoleReader {
+
+    public String waitForText(){
+        System.out.println("Write a text for reverse");
+        Scanner console = new Scanner(System.in);
+
+        String phrase = console.nextLine();
+
+        return phrase;
+    }
+
+}
---
+++ Lesson1/src/main/java/WordSplitter.java
@@ -0,0 +1,9 @@
+public class WordSplitter {
+
+    public String[] splitPhrase(String phraseToSplite){
+
+        String [] words = phraseToSplite.split(" ");
+        return words;
+    }
+
+}
